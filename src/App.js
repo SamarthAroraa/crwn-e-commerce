aa1f07e459fa9a17061477bb971daa18b0fbd01b
Samarth Arora <samartharora@Samarths-MacBook-Air.local>
2020-05-29 13:58:24 +0530

    added the page header

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
